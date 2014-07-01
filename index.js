fe09e0ffd446788f32a858591835b05c88db618f
Joseph Gentle <me@josephg.com>
2014-07-01 12:09:52 -0700

    Swapped to the uncompressed version of node-bcsocket for easier debugging

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ exports.server = require('./dist/server');
 // This exposes the bare browserchannel implementation.
 //exports.goog = require('./dist/node-browserchannel.js');
 
-var BCSocket = require('./dist/node-bcsocket.js');
+var BCSocket = require('./dist/node-bcsocket-uncompressed.js');
 exports.BCSocket = BCSocket.BCSocket;
 exports.setDefaultLocation = BCSocket.setDefaultLocation;
